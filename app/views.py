85df6c2cfa9324bb30a15c3924c3e3381035d34f
nobinov <novianhabibie@gmail.com>
2017-02-03 14:30:28 +0700

    lupa import abort

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, session, url_for, request, g, jsonify
+from flask import render_template, flash, redirect, session, url_for, request, g, jsonify, abort
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from datetime import datetime
 from app import app, db
