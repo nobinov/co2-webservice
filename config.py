772f9faaac20df8ee589a171b26a7ce24f92bc63
Ubuntu <ubuntu@ip-172-31-44-253.us-west-2.compute.internal>
2017-09-20 17:23:48 +0700

    update 20170920

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://riset1231:riset1231@c02.cxhy9bpkxdlw.us-west-2.rds.amazonaws.com:3306/co2'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://riset1231:riset1231@c02.cxhy9bpkxdlw.us-west-2.rds.amazonaws.com:3306/co2'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
